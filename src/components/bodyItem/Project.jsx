0e24f79213dcbad4bc74c83ae3a6b6b9ac77df45
Rohit <rohitmaurya152@gmail.com>
2025-02-08 12:53:58 +0530

    carousel

--- src/components/bodyItem/Project.jsx
+++ src/components/bodyItem/Project.jsx
@@ -58,44 +58,44 @@ const Project = () => {
         Check out some of our awesome projects <br /> with creative ideas and
         great design.
       </motion.h2>
-      <div className="carousel carousel-center bg-white rounded-box w-full space-x-4 p-4">
-        <div className="carousel-item w-1/3">
+      <div className="carousel carousel-center bg-white rounded-box w-full space-x-4 p-4 max-sm:w-11/12 max-sm:p-2 max-sm:mt-4">
+        <div className="carousel-item w-1/3 max-sm:w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
             className="rounded-box w-full h-80"
           />
         </div>
-        <div className="carousel-item w-1/3">
+        <div className="carousel-item w-1/3 max-sm:w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
             className="rounded-box w-full h-80"
           />
         </div>
-        <div className="carousel-item w-1/3">
+        <div className="carousel-item w-1/3 max-sm:w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
             className="rounded-box w-full h-80"
           />
         </div>
-        <div className="carousel-item w-1/3">
+        <div className="carousel-item w-1/3 max-sm:w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
             className="rounded-box w-full h-80"
           />
         </div>
-        <div className="carousel-item w-1/3">
+        <div className="carousel-item w-1/3 max-sm:w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
             className="rounded-box w-full h-80"
           />
         </div>
-        <div className="carousel-item w-1/3">
+        <div className="carousel-item w-1/3 max-sm:w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
             className="rounded-box w-full h-80"
           />
         </div>
-        <div className="carousel-item w-1/3">
+        <div className="carousel-item w-1/3 max-sm:w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
             className="rounded-box w-full h-80"
