84541b0dabf795f52e96c8201869b6cc5307451f
Rohit <rohitmaurya152@gmail.com>
2025-02-12 01:46:27 +0530

    Enhance Blog and Error pages with responsive design and added imagery

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <section className="h-80 flex justify-center items-center font-['Manrope']">
-      <h1 className="text-2xl">
+    <section className="h-80 flex justify-center items-center font-['Manrope'] flex-col gap-4 p-5">
+      <h1 className="text-2xl max-sm:text-xl max-sm:text-center">
         This page is under fabrication we will live it soon{" "}
       </h1>
+      <img
+        src="../../public/blog/Coming Soon.jpg"
+        className="h-3/4 w-1/4 max-sm:w-2/3"
+      />
     </section>
   );
 };
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router";
 const Error = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-2 font-['Manrope']">
+    <div className="flex flex-col items-center justify-center h-screen gap-2 font-['Manrope'] max-sm:p-5">
       <h1 className="text-[#343f52] text-5xl text font-semibold">404</h1>
       <h2>Page not found</h2>
-      <p className="text-sm text-[#7e8ba3]">
+      <p className="text-sm text-[#7e8ba3] max-sm:text-center">
         But don't worry, you can find plenty of other things on our homepage
       </p>
       <button
